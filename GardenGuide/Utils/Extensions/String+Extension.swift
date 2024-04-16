3ae9bebf9f86e7a201182a368cfc8d002ee238ff
DonovanZJaimes <donovanzja@gmail.com>
2024-04-16 16:27:58 -0600

    Add string extension for password and email

--- GardenGuide/Utils/Extensions/String+Extension.swift
+++ GardenGuide/Utils/Extensions/String+Extension.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+extension String {
+    init?(passwordSafeString: String) {
+        guard passwordSafeString.rangeOfCharacter(from: .uppercaseLetters) != nil && passwordSafeString.rangeOfCharacter(from: .lowercaseLetters) != nil && passwordSafeString.rangeOfCharacter(from: .punctuationCharacters) != nil && passwordSafeString.rangeOfCharacter(from: .decimalDigits) != nil else {
+            return nil
+        }
+        self = passwordSafeString
+        
+    }
+    
+    var isValidEmail: Bool {
+        let firstpart = "[A-Z0-9a-z]([A-Z0-9a-z._%+-]{0,30}[A-Z0-9a-z])?"
+        let serverpart = "([A-Z0-9a-z]([A-Z0-9a-z-]{0,30}[A-Z0-9a-z])?\\.){1,5}"
+        let emailRegex = firstpart + "@" + serverpart + "[A-Za-z]{2,6}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegex)
+        return emailTest.evaluate(with: self)
+    }
+    
+
+}
+
