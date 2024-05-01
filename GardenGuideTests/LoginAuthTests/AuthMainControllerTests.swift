d2f45dfc38165bd8fda7b24e3f416fb0004cbf62
DonovanZJaimes <donovanzja@gmail.com>
2024-05-01 16:06:07 -0600

    Add Mock and tests of AuthMainController

--- GardenGuideTests/LoginAuthTests/AuthMainControllerTests.swift
+++ GardenGuideTests/LoginAuthTests/AuthMainControllerTests.swift
@@ -6,30 +6,252 @@
 //
 
 import XCTest
+@testable import GardenGuide
 
-final class AuthMainControllerTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+@MainActor final class AuthMainControllerTests: XCTestCase {
+    var sutDelegate: AuthMainViewControllerMock!
+    var sut: AuthMainController!
+    
+    @MainActor override func setUpWithError() throws {
+        sutDelegate = AuthMainViewControllerMock()
+        sut = AuthMainController(delegate: sutDelegate)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    @MainActor override func tearDownWithError() throws {
+        sut = nil
+        sutDelegate.IsTheAuthenticationSuccessful = nil
+        sutDelegate = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func test_signUpAnonymously_CheckIfYouCanAuthenticateAnonymously() throws{ //3
+        //Arrange
+        sutDelegate.IsTheAuthenticationSuccessful = nil
+        
+        //Act
+        sutDelegate.expectation = expectation(description: "loading")
+        //Task{
+            sut.signUpAnonymously()
+        //}
+        waitForExpectations(timeout: 2.1)
+    
+        //Assert
+        guard let Authentication = sutDelegate.IsTheAuthenticationSuccessful else {
+            XCTFail("Don't have time to create an anonymous user")
+            return
         }
+        
+        XCTAssertTrue(Authentication, "could not create an anonymous account")
+    }
+    
+    
+    //MARK: SignIn
+    func test_signInWithEmail_withExistingEmailButNoPassword() { //1
+        //Code=17009
+        //Arrange
+        let error = NetworkErrorFirebase.ErrorWrongPassword.errorDescription!
+        let password: String = ""
+        let email: String = "lpa26141@vogco.com"
+        
+        //Act
+        sutDelegate.expectation = expectation(description: "ExistingEmailButNoPassword")
+        //Task{
+            sut.signInWithEmail(email, password: password)
+        //}
+        waitForExpectations(timeout: 3.1)
+            
+        //Assert
+        guard let Authentication = sutDelegate.IsTheAuthenticationSuccessful else {
+            XCTFail("There was not enough time to verify the data ")
+            return
+        }
+        
+        XCTAssertFalse(Authentication, "There is a problem with the password or email sent")
+        XCTAssertEqual(sutDelegate.error, error, "The error received does not match")
+        
+    }
+    
+    func test_signInWithEmail_withExistingEmailButIncorrectPassword() {
+        //Code=17004
+        //Arrange
+        let error = NetworkErrorFirebase.ErrorInvalidPassword.errorDescription!
+        let password: String = "4@d"
+        let email: String = "lpa26141@vogco.com"
+        
+        //Act
+        sutDelegate.expectation = expectation(description: "ExistingEmailButIncorrectPassword")
+        //Task{
+            sut.signInWithEmail(email, password: password)
+        //}
+        waitForExpectations(timeout: 3.2)
+            
+        //Assert
+        guard let Authentication = sutDelegate.IsTheAuthenticationSuccessful else {
+            XCTFail("There was not enough time to verify the data ")
+            return
+        }
+        
+        XCTAssertFalse(Authentication, "There is a problem with the password or email sent")
+        XCTAssertEqual(sutDelegate.error, error, "The error received does not match")
+    }
+    
+    func test_signInWithEmail_withIncorrectEmailAndFullPassword() { //2
+        //Code=17008
+        //Arrange
+        let error = NetworkErrorFirebase.ErrorInvalidEmail.errorDescription!
+        let password: String = "4@dWert3Ade3"
+        let email: String = "lpa26141@vogco."
+        
+        //Act
+        sutDelegate.expectation = expectation(description: "IncorrectEmailAndFullPassword")
+        //Task{
+            sut.signInWithEmail(email, password: password)
+       // }
+        waitForExpectations(timeout: 4.0)
+            
+        //Assert
+        guard let Authentication = sutDelegate.IsTheAuthenticationSuccessful else {
+            XCTFail("There was not enough time to verify the data ")
+            return
+        }
+        
+        XCTAssertFalse(Authentication, "There is a problem with the password or email sent")
+        XCTAssertEqual(sutDelegate.error, error, "The error received does not match")
+    }
+    
+    
+    
+    //MARK: SignUp
+    func test_signUpWithEmail_withEmailAndSmallPassword() throws {
+        //Code=17026
+        //Arrange
+        let error = NetworkErrorFirebase.ErrorWeakPassword.errorDescription!
+        let password: String = "gF4@"
+        let email: String = "gqc92362@fosiq.com"
+        
+        //Act
+        sutDelegate.expectation = expectation(description: "loading Small Password ")
+        //Task{
+            sut.signUpWithEmail(email, password: password)
+        //}
+        waitForExpectations(timeout: 3.1)
+            
+        //Assert
+        guard let Authentication = sutDelegate.IsTheAuthenticationSuccessful else {
+            XCTFail("There was not enough time to verify the data ")
+            return
+        }
+        
+        XCTAssertFalse(Authentication, "There is a problem with the password or email sent")
+        XCTAssertEqual(sutDelegate.error, error, "The error received does not match")
+        
+    }
+    
+    func test_signUpWithEmail_withoutEmailAndPassword() throws {
+        //Code=17026
+        //Arrange
+        let error = NetworkErrorFirebase.ErrorWeakPassword.errorDescription!
+        let password: String = ""
+        let email: String = ""
+        
+        //Act
+        sutDelegate.expectation = expectation(description: "withoutEmailAndPassword")
+        //Task{
+            sut.signUpWithEmail(email, password: password)
+        //}
+        waitForExpectations(timeout: 3.1)
+            
+        //Assert
+        guard let Authentication = sutDelegate.IsTheAuthenticationSuccessful else {
+            XCTFail("There was not enough time to verify the data ")
+            return
+        }
+        
+        XCTAssertFalse(Authentication, "There is a problem with the password or email sent")
+        XCTAssertEqual(sutDelegate.error, error, "The error received does not match")
+        
+    }
+    
+    func test_signUpWithEmail_withAuthenticatedEmailAndWrongPassword() throws { //1
+        //Code=17007
+        
+        //Arrange
+        let error = NetworkErrorFirebase.ErrorEmailAlredyInUse.errorDescription!
+        let password: String = "4#Adui"
+        let email: String = "lpa26141@vogco.com"
+        
+        //Act
+        sutDelegate.expectation = expectation(description: "withAuthenticatedEmailAndWrongPassword")
+        //Task{
+            sut.signUpWithEmail(email, password: password)
+        //}
+        waitForExpectations(timeout: 3.5)
+            
+        //Assert
+        guard let Authentication = sutDelegate.IsTheAuthenticationSuccessful else {
+            XCTFail("There was not enough time to verify the data ")
+            return
+        }
+        
+        XCTAssertFalse(Authentication, "There is a problem with the password or email sent")
+        XCTAssertEqual(sutDelegate.error, error, "The error received does not match")
+        
+    }
+    
+    func test_signUpWithEmail_withoutEmailAndWithPassword() throws {
+        //Code=17034
+        
+        //Arrange
+        let error = NetworkErrorFirebase.ErrorMissingEmail.errorDescription!
+        let password: String = "4#Adui"
+        let email: String = ""
+        
+        //Act
+        sutDelegate.expectation = expectation(description: "withoutEmailAndWithPassword")
+        //Task{
+            sut.signUpWithEmail(email, password: password)
+        //}
+        waitForExpectations(timeout: 3.5)
+            
+        //Assert
+        guard let Authentication = sutDelegate.IsTheAuthenticationSuccessful else {
+            XCTFail("There was not enough time to verify the data ")
+            return
+        }
+        
+        XCTAssertFalse(Authentication, "There is a problem with the password or email sent")
+        XCTAssertEqual(sutDelegate.error, error, "The error received does not match")
+        
+    }
+    
+    func test_signUpWithEmail_withBadEmailAndGoodPassword() throws {
+        //Code=17008
+        
+        //Arrange
+        let error = NetworkErrorFirebase.ErrorInvalidEmail.errorDescription!
+        let password: String = "4#Adui"
+        let email: String = "asdf5@hjh."
+        
+        //Act
+        sutDelegate.expectation = expectation(description: "withBadEmailAndGoodPassword")
+        //Task{
+            sut.signUpWithEmail(email, password: password)
+        //}
+        waitForExpectations(timeout: 4.5)
+            
+        //Assert
+        guard let Authentication = sutDelegate.IsTheAuthenticationSuccessful else {
+            XCTFail("There was not enough time to verify the data ")
+            return
+        }
+        
+        XCTAssertFalse(Authentication, "There is a problem with the password or email sent")
+        XCTAssertEqual(sutDelegate.error, error, "The error received does not match")
+        
     }
 
 }
+
+
+
+
--- GardenGuideTests/LoginAuthTests/Mocks/AuthMainViewControllerMock.swift
+++ GardenGuideTests/LoginAuthTests/Mocks/AuthMainViewControllerMock.swift
@@ -4,5 +4,31 @@
 //
 //  Created by Donovan Z. Jaimes on 24/04/24.
 //
+import XCTest
+@testable import GardenGuide
 
-import Foundation
+
+class AuthMainViewControllerMock: AuthMainViewProtocol {
+    var IsTheAuthenticationSuccessful: Bool? = nil
+    var provider: ProviderType!
+    var email: String!
+    var expectation: XCTestExpectation?
+    var error: String!
+    
+    func successfulAuthentication(provider: GardenGuide.ProviderType, email: String) {
+        IsTheAuthenticationSuccessful = true
+        self.provider = provider
+        self.email = email
+        expectation?.fulfill()
+        
+    }
+    
+    func failedAuthentication(error: String) {
+        IsTheAuthenticationSuccessful = false
+        self.error = error
+        expectation?.fulfill()
+    }
+    
+    
+}
+
