d25a13a4223bc240627d7e6cc5e1a7f04cb531d5
DonovanZJaimes <donovanzja@gmail.com>
2024-05-07 16:37:52 -0600

    Modify color extension

--- GardenGuide/Utils/Extensions/UIColor+Extension.swift
+++ GardenGuide/Utils/Extensions/UIColor+Extension.swift
@@ -11,5 +11,27 @@ import UIKit
 extension UIColor {
     @nonobjc class var opaqueGray: UIColor {
         UIColor(named: "OpaqueGray")!
-            }
+    }
+    @nonobjc class var customDarkGreen: UIColor {
+        UIColor(named: "CustomDarkGreen")!
+    }
+    @nonobjc class var customGreen: UIColor {
+        UIColor(named: "CustomGreen")!
+    }
+    @nonobjc class var customGreenishGray: UIColor {
+        UIColor(named: "CustomGreenishGray")!
+    }
+    @nonobjc class var customLightGreen: UIColor {
+        UIColor(named: "CustomLightGreen")!
+    }
+    @nonobjc class var customOpaqueGreen: UIColor {
+        UIColor(named: "CustomOpaqueGreen")!
+    }
+    @nonobjc class var customYellow: UIColor {
+        UIColor(named: "CustomYellow")!
+    }
+    @nonobjc class var customWhite: UIColor {
+        UIColor(named: "CustomWhite")!
+    }
+    
 }
