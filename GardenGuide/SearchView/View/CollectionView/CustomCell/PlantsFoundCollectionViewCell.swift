06d29df353049a59a39e5f381d4c2db7654be413
DonovanZJaimes <donovanzja@gmail.com>
2024-05-11 16:34:54 -0600

    Add the layout and view of the CollectionViewCell to the SearchView

--- GardenGuide/SearchView/View/CollectionView/CustomCell/PlantsFoundCollectionViewCell.swift
+++ GardenGuide/SearchView/View/CollectionView/CustomCell/PlantsFoundCollectionViewCell.swift
@@ -9,9 +9,28 @@ import UIKit
 
 class PlantsFoundCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var addPlantButton: UIButton!
+    @IBOutlet weak var plantImage: UIImageView!
+    @IBOutlet weak var namePlantLabel: UILabel!
+    @IBOutlet weak var probabilityLabel: UILabel!
+    @IBOutlet weak var backgroundInfoView: UIView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        backgroundInfoView.layer.cornerRadius = backgroundInfoView.frame.height / 4
+        plantImage.layer.cornerRadius = plantImage.frame.width / 10
     }
-
+    
+    func configureCell(plant: SuggestedPlant) {
+        namePlantLabel.text = plant.name
+        probabilityLabel.text = "\(plant.probability)"
+        
+    }
+    
+    
+    
+    @IBAction func addPlantToFavorites(_ sender: UIButton) {
+        addPlantButton.isSelected.toggle()
+    }
+    
 }
