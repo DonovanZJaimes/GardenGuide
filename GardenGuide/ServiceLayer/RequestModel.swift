69c7205e2ee8cab7dfbe91302b7b4ef2b68fb29c
DonovanZJaimes <donovanzja@gmail.com>
2024-05-13 16:50:40 -0600

    Add Kingfisher package dependencies

---
+++ GardenGuide/ServiceLayer/RequestModel.swift
@@ -0,0 +1,8 @@
+//
+//  RequestModel.swift
+//  GardenGuide
+//
+//  Created by Donovan Z. Jaimes on 13/05/24.
+//
+
+import Foundation
---
+++ GardenGuide/ServiceLayer/ServiceLayer.swift
@@ -0,0 +1,8 @@
+//
+//  ServiceLayer.swift
+//  GardenGuide
+//
+//  Created by Donovan Z. Jaimes on 13/05/24.
+//
+
+import Foundation
