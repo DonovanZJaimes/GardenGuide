d5cbb78bf32ca65a7a63fc7ee8f84d78b13f32c5
DonovanZJaimes <donovanzja@gmail.com>
2024-06-13 20:39:02 -0600

    Add the irrigation model to the general model

---
+++ GardenGuide/Model/Irrigation/IrrigationInformation.swift
@@ -0,0 +1,16 @@
+//
+//  IrrigationInformation.swift
+//  GardenGuide
+//
+//  Created by Donovan Z. Jaimes on 12/06/24.
+//
+
+import Foundation
+
+struct IrrigationInformation {
+    var numberOfDays: Int16
+    var waterAmount: Int16
+    var percentage: Double
+    var wasItWatered: Bool
+    var nextIrrigation: Date
+}
--- GardenGuide/Model/PlantsPerImage/SimilarImage.swift
+++ GardenGuide/Model/PlantsPerImage/SimilarImage.swift
@@ -11,10 +11,12 @@ struct SimilarImage: Decodable {
     let id: String
     let url: String
     let similarity: Double
+    let image: Data?
 
     enum CodingKeys: String, CodingKey {
         case id
         case url
         case similarity
+        case image
     }
 }
