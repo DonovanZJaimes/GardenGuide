62b8b2916c399299949384e931493f13768f000c
Donovan Z. Jaimes <donovanzja@gmail.com>
2024-12-11 20:07:38 -0600

    Add files to Cloud Firestore

--- GardenGuide/Firebase/CloudFirestore/FirestoreAddData.swift
+++ GardenGuide/Firebase/CloudFirestore/FirestoreAddData.swift
@@ -6,3 +6,98 @@
 //
 
 import Foundation
+import FirebaseFirestore
+
+struct FirestoreAddData {
+    //Cloud Firestore
+    static var shared =  FirestoreAddData()
+    private let db = Firestore.firestore()
+    private var firestoreUtilts = FirestoreUtilts()
+    private let CoUser = FirestoreUtilts.collectionUsers
+    private let CoFavPlants = FirestoreUtilts.collectionFavouritePlants
+    private let CoGarPlants = FirestoreUtilts.collectionGardenPlants
+    private let CoSimImage = FirestoreUtilts.collectionSimilarImage
+    private let CoIrrInfo = FirestoreUtilts.collectionIrrigationInformation
+    private let CoDetInfo = FirestoreUtilts.collectionDetailsInformation
+    
+    //MARK: Methods for storing user information in the Firestore Cloud
+    
+    //Saving user
+    mutating func addUserToCloud(with email: String, andProvider provider: String) async {
+        do {
+            try await db.collection(CoUser).document(email).setData([
+            "provider": provider
+        ])
+            print("user save on Firestore Cloud")
+            firestoreUtilts.modifyUserEmail(email)
+        } catch {
+          print("Error adding user: \(error)")
+        }
+    }
+    
+    //Saving favourite plants of the user
+    func addPlantOfFavouritesToCloud(_ name: String, image: String) async {
+        do {
+            //save name and image of plant
+            try await db.collection(CoUser).document(FirestoreUtilts.userEmail).collection(CoFavPlants).document(name).setData([
+                "name" : name,
+                "image" : image
+            ])
+            print("save plant on Firestore Cloud")
+        } catch {
+          print("Error adding user plant: \(error)")
+        }
+    }
+    
+    //Saving garden plants of the user
+    func addGardenPlantToCloud(_ plant: PlantForFirestore) async {
+        let emptyArray = [""]
+        do {
+            //save name of the plant
+            try await db.collection(CoUser).document(FirestoreUtilts.userEmail).collection(CoGarPlants).document(plant.name).setData([
+                "name" : plant.name,
+                "isAdded" : plant.isAdded
+            ])
+            
+            //save plant details
+            try await db.collection(CoUser).document(FirestoreUtilts.userEmail).collection(CoGarPlants).document(plant.name).collection(CoDetInfo).document(CoDetInfo).setData([
+                "commonNames" : (plant.details.commonNames ?? emptyArray),
+                "url" : (plant.details.url ?? ""),
+                "rank" : (plant.details.rank ?? ""),
+                "descriptionValue" : (plant.details.descriptionValue ?? ""),
+                "descriptionUrl" : (plant.details.descriptionUrl ?? ""),
+                "synonyms" : (plant.details.synonyms ?? emptyArray),
+                "imageUrl" : (plant.details.imageUrl ?? ""),
+                "edibleParts" : (plant.details.edibleParts ?? emptyArray),
+                "wateringMax" : (plant.details.wateringMax ?? 0),
+                "wateringMin" : (plant.details.wateringMin ?? 0),
+                "propagationMethods" : (plant.details.propagationMethods ?? emptyArray)
+                
+            ])
+            //Save similar images of the plant
+            if let similarImages = plant.similarImages {
+                for index in 1 ... similarImages.count {
+                    let documentSimilarImage = CoSimImage + String(index)
+                    try await db.collection(CoUser).document(FirestoreUtilts.userEmail).collection(CoGarPlants).document(plant.name).collection(CoSimImage).document(documentSimilarImage).setData([
+                        "id" : similarImages[index - 1].id,
+                        "url" : similarImages[index - 1].url,
+                        "similarity" :  similarImages[index - 1].similarity
+                    
+                    ])
+                }
+            }
+            //Save the irrigation information of the plant
+            try await db.collection(CoUser).document(FirestoreUtilts.userEmail).collection(CoGarPlants).document(plant.name).collection(CoIrrInfo).document(CoIrrInfo).setData([
+                "numberOfDays" : plant.watered.numberOfDays,
+                "waterAmount" : plant.watered.waterAmount,
+                "percentage" : plant.watered.percentage,
+                "wasItWatered" : plant.watered.wasItWatered,
+                "nextIrrigation" : plant.watered.nextIrrigation
+            ])
+            
+            print("save garden plant on Firestore Cloud")
+        } catch {
+          print("Error save garden plant: \(error)")
+        }
+    }
+}
--- GardenGuide/Firebase/CloudFirestore/FirestoreUtilts.swift
+++ GardenGuide/Firebase/CloudFirestore/FirestoreUtilts.swift
@@ -6,3 +6,83 @@
 //
 
 import Foundation
+
+struct FirestoreUtilts {
+    //Constants of collections in Cloud Firestore
+    static var userEmail = ""
+    static var userProvider : ProviderType = .none
+    static var shared =  FirestoreUtilts()
+    static let collectionUsers = "users"
+    static let collectionFavouritePlants = "FavouritePlants"
+    static let collectionGardenPlants = "GardenPlants"
+    static let collectionSimilarImage = "SimilarImage"
+    static let collectionIrrigationInformation = "IrrigationInformation"
+    static let collectionDetailsInformation = "DetailsInformation"
+}
+
+extension FirestoreUtilts {
+    //MARK: Change the user information in the application
+    mutating func modifyUserEmail(_ email: String){
+        FirestoreUtilts.userEmail = email
+    }
+    mutating func modifyUserProvider(_ provider: String){
+        guard let providerType = ProviderType.init(rawValue: provider) else  { return}
+        FirestoreUtilts.userProvider = providerType
+    }
+    
+    //MARK: Methods for changing from one model to another
+    
+    //PlantInformation To PlantForFirestore
+    public func plantInformationModelToPlantForFirestoreModel(_ plant: PlantInformation, watered: IrrigationInformation) -> PlantForFirestore {
+        let name = plant.name
+        let isAdded = true
+        let similarImages = plant.similarImages
+        let details = plantDetailsModelToPlantDetailsFirestoreModel(plant.details)
+        let watered = watered
+        let plantForFirestore = PlantForFirestore(name: name, isAdded: isAdded, similarImages: similarImages, details: details, watered: watered)
+        return plantForFirestore
+    }
+    
+    //PlantForFirestore To PlantInformation
+    public func plantForFirestoreModelToPlantInformationModel(_ plant: PlantForFirestore) -> (PlantInformation, IrrigationInformation){
+        //IrrigationInformation
+        let irrigationInformation = plant.watered
+        //PlantDetails
+        let name = plant.name
+        let isAdded = true
+        let similarImages = plant.similarImages
+        let details = plantDetailsFirestoreModelToPlantDetailsModel(plant.details)
+        let plantInformation = PlantInformation(name: name, isAdded: isAdded, similarImages: similarImages, details: details)
+       return (plantInformation, irrigationInformation)
+    }
+    
+    //PlantDetails To PlantDetailsFirestore
+    private func plantDetailsModelToPlantDetailsFirestoreModel(_ model: PlantDetails) -> PlantDetailsFirestore {
+        let plantDetailsFirestore = PlantDetailsFirestore(commonNames: model.commonNames, url: model.url, rank: model.rank, descriptionUrl: model.description?.url, descriptionValue: model.description?.value, synonyms: model.synonyms, imageUrl: model.image?.url, edibleParts: model.edibleParts, wateringMax: model.watering?.max, wateringMin: model.watering?.min, propagationMethods: model.propagationMethods)
+        return plantDetailsFirestore
+    }
+    
+    //PlantDetailsFirestore To PlantDetails
+    private func plantDetailsFirestoreModelToPlantDetailsModel(_ model: PlantDetailsFirestore) -> PlantDetails {
+        var description = PlantDetails.Description(value: "", url: "")
+        var image = PlantDetails.DescriptionImage(url: "", image: nil)
+        if let value = model.descriptionValue, let url = model.descriptionUrl, let imageUrl = model.imageUrl {
+            description = PlantDetails.Description(value: value, url: url)
+            image = PlantDetails.DescriptionImage(url: imageUrl, image: nil)
+        }
+        let watered = Watered(max: model.wateringMax ?? 0, min: model.wateringMin ?? 0)
+        let plantDetails = PlantDetails(commonNames: model.commonNames, url: model.url, rank: model.rank, description: description, synonyms: model.synonyms, image: image, edibleParts: model.edibleParts, watering: watered, propagationMethods: model.propagationMethods)
+        return plantDetails
+    }
+    
+}
+
+
+//MARK: Method to know if Firestore Cloud can be used
+public func makeMethodsForFirestoreCloud(callback: @escaping () async ->Void ) async {
+    let userProvider = FirestoreUtilts.userProvider
+    guard userProvider != ProviderType.anonymous && userProvider != ProviderType.none else {
+        print("You need to hava an account to save the plant on Firestore")
+        return }
+    await callback()
+}
