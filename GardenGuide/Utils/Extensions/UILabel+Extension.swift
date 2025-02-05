e8b9fdfa50749465124c9d4039791059d8ecafda
DonovanZJaimes <https://github.com/DonovanZJaimes>
2025-02-05 16:28:04 -0600

    Add new UILabel extension

--- GardenGuide/Utils/Extensions/UILabel+Extension.swift
+++ GardenGuide/Utils/Extensions/UILabel+Extension.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import UIKit
+extension UILabel {
+    public func underlineStyle(color: UIColor ) {
+        guard let text = self.text else { return }
+        // Create the underline attribute with color
+        let attributes: [NSAttributedString.Key: Any] = [
+            .underlineStyle: NSUnderlineStyle.single.rawValue,
+            .underlineColor: color
+        ]
+
+        // Create the NSAttributedString with the attribute
+        let attributedString = NSAttributedString(string: text, attributes: attributes)
+
+        // Assign the NSAttributedString to the label
+        attributedText = attributedString
+    }
+    
+}
