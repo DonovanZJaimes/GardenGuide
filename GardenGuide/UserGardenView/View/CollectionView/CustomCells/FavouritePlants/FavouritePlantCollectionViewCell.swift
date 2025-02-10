6511d4ced0c6bf22581c3b2ca45fe82d54a1bd16
DonovanZJaimes <donovanzja@gmail.com>
2025-02-09 20:29:34 -0600

    Add FavouritePlantCell in UserGardenView

--- GardenGuide/UserGardenView/View/CollectionView/CustomCells/FavouritePlants/FavouritePlantCollectionViewCell.swift
+++ GardenGuide/UserGardenView/View/CollectionView/CustomCells/FavouritePlants/FavouritePlantCollectionViewCell.swift
@@ -6,12 +6,47 @@
 //
 
 import UIKit
+import Kingfisher
+//MARK: Protocol...
+protocol FavouritePlantCollectionViewCellDelegate:AnyObject {
+    func favouritePlantConfiguration(favouritePlant: FavouritePlant)
+}
 
 class FavouritePlantCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var plantImage: UIImageView!
+    @IBOutlet weak var plantNameLabel: UILabel!
+    
+    weak var delegate: FavouritePlantCollectionViewCellDelegate?
+    var favouritePlant: FavouritePlant!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        updateImage(cornerRadius: 30)
     }
+    
+    //MARK: Methods to Cell
+    func updateImage(cornerRadius: CGFloat) {
+        //Configuration of the plant image design
+        let borderColor: UIColor = .customDarkGreen
+        plantImage.modifyCornerRadius(cornerRadius - 1, withColor: borderColor, andWidth: 0.3)
+    }
+    
+    func updateCell(name: String, image: String, cornerRadius: CGFloat, favouritePlant: FavouritePlant){
+        self.favouritePlant = favouritePlant
+        //label
+        plantNameLabel.text = favouritePlant.name
+        //Image
+        guard let urlImage = URL(string: favouritePlant.image) else { return }
+        plantImage.kf.setImage(with: urlImage)
+        updateImage(cornerRadius: cornerRadius)
+    }
+    
+    @IBAction func plantConfiguration(_ sender: UIButton) {
+        //send instructions to edit the selected plant
+        delegate?.favouritePlantConfiguration(favouritePlant: self.favouritePlant)
+    }
+    
 
 }
