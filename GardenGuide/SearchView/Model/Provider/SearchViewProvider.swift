53519e1236ced8f40a1f3fb907cacb78f4e23d81
DonovanZJaimes <donovanzja@gmail.com>
2025-02-12 13:10:09 -0600

    Add model Provider in SearchView

--- GardenGuide/SearchView/Model/Provider/SearchViewProvider.swift
+++ GardenGuide/SearchView/Model/Provider/SearchViewProvider.swift
@@ -12,30 +12,35 @@ protocol SearchViewProviderProtocol {
     func getPlantsByText(_ text: String) async throws -> [SuggestedPlantName]
     
 } 
+//MARK: Provider to get information to the API
 class SearchViewProvider: SearchViewProviderProtocol {
+    //Get an array SuggestedPlantName model from the API
     func getPlantsByText(_ text: String) async throws -> [SuggestedPlantName] {
+        //network application information
         let queryItem = ["q": text]
         let requestModel = RequestModel(endpoint: .plants, httpMethod: .GET, queryItems: queryItem, postData: nil)
         
         do {
+            //get data
             let model = try await ServiceLayer.callService(requestModel, SimilarPlantsName.self)
             let suggestedPlantsName = model.entities
             return suggestedPlantsName
-            
-            
         }catch {
             throw NetworkError.generic
         }
        
     }
     
+    //Get a PlantsByImageSearch model from the API
     func getPlantsByImage(imageInBase64String: String) async throws -> PlantsByImageSearch {
+        //network application information
         let parameters = "{\n    \"images\": [\"data:image/jpg;base64," + imageInBase64String + "\"],\n   \"similar_images\": true\n}"
         let postData = parameters.data(using: .utf8)
         let queryItem = ["details":"common_names,url,description,taxonomy,rank,gbif_id,inaturalist_id,image,synonyms,edible_parts,watering,propagation_methods"]
         let requestModel = RequestModel(endpoint: .identification, httpMethod: .POST, queryItems: queryItem, postData: postData)
         
         do {
+            //get data
             let model = try await ServiceLayer.callService(requestModel, SimilarPlants.self)
             let isPlant = model.result.isPlant
             let suggestedPlants = model.result.classification.suggestedPlants
@@ -48,11 +53,14 @@ class SearchViewProvider: SearchViewProviderProtocol {
         
     }
     
+    //Get a PlantDetails model from the API
     func getPlantDetails(accessToken: String) async throws -> PlantDetails {
+        //network application information
         let queryItem = ["details":"common_names,url,description,taxonomy,rank,gbif_id,inaturalist_id,image,synonyms,edible_parts,watering,propagation_methods"]
         let requestModel = RequestModel(endpoint: .detail, httpMethod: .GET, queryItems: queryItem, extra: accessToken, postData: nil)
         
         do {
+            //get data
             let model = try await ServiceLayer.callService(requestModel, PlantDetails.self)
             return model
         } catch {
--- GardenGuide/SearchView/Model/Provider/SearchViewProviderMock.swift
+++ GardenGuide/SearchView/Model/Provider/SearchViewProviderMock.swift
@@ -7,19 +7,24 @@
 
 import Foundation
 
+//MARK: Provider to get information to the Mock
 class SearchViewProviderMock: SearchViewProviderProtocol {
+    //Get an array SuggestedPlantName model from the Mock
     func getPlantsByText(_ text: String) async throws -> [SuggestedPlantName] {
+        //Get data with mock .json
         guard let model = Utils.parseJson(jsonName: "PlantNameResultsUsingTextSearch", model: SimilarPlantsName.self) else{
-            print("No se pudo convertir")
+            print("Could not be converted")
             throw NetworkError.jsonDecoder
         }
         let suggestedPlantsName = model.entities
         return suggestedPlantsName
     }
     
+    //Get a PlantsByImageSearch model from the Mock
     func getPlantsByImage(imageInBase64String: String) async throws -> PlantsByImageSearch {
+        //Get data with mock .json
         guard let model = Utils.parseJson(jsonName: "PlantResultsByImageSearch", model: SimilarPlants.self) else{
-            print("No se pudo convertir")
+            print("Could not be converted")
             throw NetworkError.jsonDecoder
         }
         let isPlant = model.result.isPlant
@@ -29,9 +34,11 @@ class SearchViewProviderMock: SearchViewProviderProtocol {
         return plantsByImageSearch
     }
     
+    //Get a PlantDetails model from the Mock
     func getPlantDetails(accessToken: String) async throws -> PlantDetails {
+        //Get data with mock .json
         guard let model = Utils.parseJson(jsonName: "??", model: PlantDetails.self) else{
-            print("No se pudo convertir")
+            print("Could not be converted")
             throw NetworkError.jsonDecoder
         }
         return model
